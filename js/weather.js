148c3954d3e7e55f0e5821747f69e53487633e57
forc17c <85665007+forc17c@users.noreply.github.com>
2021-07-12 05:22:54 +0900

    WEATHER

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "4c106918396eb5fbabb9d23a5f2106db";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
